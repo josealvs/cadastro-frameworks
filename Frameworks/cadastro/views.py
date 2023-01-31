562ab038c6b585923ba4e916dceeb83f271fe3fd
josealvs <j.anjos@escolar.ifrn.edu.br>
2023-01-31 15:07:15 -0300

    primeiro commit

---
+++ Frameworks/cadastro/views.py
@@ -0,0 +1,62 @@
+from django.shortcuts import render, redirect
+from .models import Framework
+from .forms import frameworkForm
+
+# Create your views here.
+
+def index(request):
+    return render(request, 'index.html')
+
+
+def crud(request):
+    framework = Framework.objects.all()
+
+    form = frameworkForm()
+
+    context = {
+        'form': form
+    }
+
+    return render(request, 'crud.html', context)
+
+
+def create(request):
+    form = frameworkForm(request.POST or None)
+
+    if form.is_valid():
+        form.save()
+        return redirect('list')
+
+
+def read(request):
+
+    frameworks = Framework.objects.all()
+
+    return render(request, 'listagem.html', {'frameworks': frameworks})
+
+def consulta(request):
+
+    frameworks = Framework.objects.all()
+
+    return render(request, 'consulta.html', {'frameworks': frameworks})
+
+def search(request):
+
+    frameworks = Framework.objects.filter(name__icontains=request.GET.get('search', ''), )
+
+    return render(request, 'consulta.html', {'frameworks': frameworks})
+
+def update(request, id):
+
+    frameworks = Framework.objects.get(id = id)
+    form = frameworkForm(request.POST or None, instance = frameworks)
+    if form.is_valid():
+        form.save()
+        return redirect('list')
+    
+    return render(request, 'update.html', {'frameworks' : frameworks, 'form' : form})
+
+def delete(request, id):
+    frameworks = Framework.objects.get(id=id)
+    frameworks.delete()
+    return redirect('list')
